80d0e6925a5749eb11653b122b7513aa25576cd7
Miguel Angel Inga Ninahuanca <miguel.inga@tecsup.edu.pe>
2022-05-08 23:27:35 -0500

    Laboratorio07_MVVM_Categoria_CRUD

---
+++ Semana05/View/ListaCategoria.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+using Semana05.ViewModel;
+
+namespace Semana05.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        ListaCategoriaViewModel viewModel;
+        public ListaCategoria()
+        {
+            InitializeComponent();
+            viewModel = new ListaCategoriaViewModel();
+            this.DataContext = viewModel;
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria Bcategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = Bcategoria.Listar(0);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManCategoria manCategoria = new ManCategoria();
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        //private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    int idCategoria;
+        //    var item = (Categoria)dgvCategoria.SelectedItem;
+        //    if (null == item) return;
+        //    idCategoria = Convert.ToInt32(item.IdCategoria);
+        //    ManCategoria manCategoria = new ManCategoria();
+        //    manCategoria.ShowDialog();
+        //    Cargar();
+        //}        //private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    int idCategoria;
+        //    var item = (Categoria)dgvCategoria.SelectedItem;
+        //    if (null == item) return;
+        //    idCategoria = Convert.ToInt32(item.IdCategoria);
+        //    ManCategoria manCategoria = new ManCategoria();
+        //    manCategoria.ShowDialog();
+        //    Cargar();
+        //}
+    }
+}
---
+++ Semana05/ViewModel/ListaCategoriaViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Semana05.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+
+        public ICommand NuevoCommand { set; get; }
+
+        public ICommand ConsultarCommand { set; get; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+
+            ConsultarCommand = new RelayCommand<object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                 window.ShowDialog();
+            }
+        }
+
+    }
+}
